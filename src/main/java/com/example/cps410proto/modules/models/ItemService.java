fa071d26f2ba077e0d768adc90c24c97d866f1f8
Akram Moustafa <moust1am@cmich.edu>
2023-11-02 05:13:05 -0400

    Item Controller class and new class Item service push

---
+++ src/main/java/com/example/cps410proto/modules/models/ItemService.java
@@ -0,0 +1,37 @@
+package com.example.cps410proto.modules.models;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/*
+ *  Service class works with the auction item class to help in interacting with the user by adding and deleting items.
+ *  The user interaction is defined in ItemController class
+ */
+@Service
+public class ItemService {
+    private Map<String, AuctionItem> auctionItemMap;
+
+    public ItemService() {
+        auctionItemMap = new HashMap<>();
+    }
+
+    public AuctionItem addItem(AuctionItem item) {
+        auctionItemMap.put(item.getName(), item);
+        return item;
+    }
+    public AuctionItem removeItem(AuctionItem item) {
+        auctionItemMap.remove(item.getName(), item);
+        return item;
+    }
+
+    public AuctionItem getItemByName(String itemName) {
+        return auctionItemMap.get(itemName);
+    }
+
+    public Map<String, AuctionItem> getAllItems() {
+        return auctionItemMap;
+    }
+}
