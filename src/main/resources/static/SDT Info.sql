d3c99a1502bf9bf00ffb46b29cfb5afe4ff7fc49
simdi444 <139267957+simdi444@users.noreply.github.com>
2023-11-20 11:09:45 -0500

    Update SDT Info.sql
    Finished the insert tables for the items and the user info. Also commented out the account info for now

--- src/main/resources/static/SDT Info.sql
+++ src/main/resources/static/SDT Info.sql
@@ -1,17 +1,33 @@
 
-insert into LOGIN_INFO values ('simdi444', 'seniordesign');
-insert into LOGIN_INFO values ('user123', 'abcde');
-insert into LOGIN_INFO values ('user321', 'edcba');
-insert into LOGIN_INFO values ('Johnny', 'raft');
+insert into USER_INFO values ('simdi444', 'seniordesign');
+insert into USER_INFO values ('user123', 'abcde');
+insert into USER_INFO values ('user321', 'edcba');
+insert into USER_INFO values ('Johnny', 'raft');
 
-insert into USER_INFO values ('Simdi', 'Agomoh', 'agomo1cc@cmich.edu', 'M');
-insert into USER_INFO values ('Brock', 'Jones', 'brj@cmich.edu', 'M');
-insert into USER_INFO values ('User', 'Name', 'user@cmich.edu', 'M');
-insert into USER_INFO values ('User2', 'Name2', 'user2@cmich.edu', 'F');
-insert into USER_INFO values ('User3', 'Name3', 'user3@cmich.edu', 'F');
-insert into USER_INFO values ('Akram', 'Moustafa', 'akm.edu', 'M');
+--Commented out block for account info
+--insert into ACC_INFO values ('Simdi', 'Agomoh');
+--insert into ACC_INFO values ('Brock', 'Jones');
+--insert into ACC_INFO values ('User', 'Name');
+--insert into ACC_INFO values ('User2', 'Name2');
+--insert into ACC_INFO values ('User3', 'Name3');
+--insert into ACC_INFO values ('Akram', 'Moustafa');
 
 
-insert into ITEM_INFO values ('Vintage Couch', 800, 'Brown', 1995);
-insert into ITEM_INFO values ('Clock', 40, 'Black', 2001);
-insert into ITEM_INFO values ('Macbook', 400, 'Grey', 2003);
+insert into ITEM_INFO values ('Vintage Couch', 'Fairly used' 800, 'Brown', 1995, to_date(('2023-12-9'),('YYYY-MM-DD')), to_date(('2023-12-15'),('YYYY-MM-DD')));
+insert into ITEM_INFO values ('Clock', 'New', 115, 'Brown', 2001, to_date(('2023-12-10'),('YYYY-MM-DD')), to_date(('2023-12-20'),('YYYY-MM-DD')));
+insert into ITEM_INFO values ('Macbook', 'New', 400, 'Grey', 2005, to_date(('2023-12-15'),('YYYY-MM-DD')), to_date(('2023-12-25'),('YYYY-MM-DD')));
+insert into ITEM_INFO values ('Necklace', 'Used', 400, 'Silver', 1997, to_date(('2024-01-1'),('YYYY-MM-DD')), to_date(('2022-01-15'),('YYYY-MM-DD')));
+insert into ITEM_INFO values ('Avirex Jacket', 'Used',  200, 'Brown', 2001, to_date(('2023-12-14'),('YYYY-MM-DD')), to_date(('2023-12-21'),('YYYY-MM-DD')));
+insert into ITEM_INFO values ('Suitcases', 'New', 250, 'Brown', 1998, to_date(('2023-12-10'),('YYYY-MM-DD')), to_date(('2023-12-10'),('YYYY-MM-DD')));
+insert into ITEM_INFO values ('Vinyl player', 'Fairly Used', 130, 'Silver', 1994, to_date(('2023-12-10'),('YYYY-MM-DD')), to_date(('2023-12-10'),('YYYY-MM-DD')));
+insert into ITEM_INFO values ('Coffee grinder', 'New', 95, 'Silver', 1980, to_date(('2023-12-10'),('YYYY-MM-DD')), to_date(('2023-12-10'),('YYYY-MM-DD')));
+insert into ITEM_INFO values ('Credenza', 'New', 220, 'Brown', 2000, to_date(('2023-12-10'),('YYYY-MM-DD')), to_date(('2023-12-10'),('YYYY-MM-DD')));
+insert into ITEM_INFO values ('Dish set', 'New', 75, 'White', 1997, to_date(('2023-12-10'),('YYYY-MM-DD')), to_date(('2023-12-10'),('YYYY-MM-DD')));
+insert into ITEM_INFO values ('Camera', 'Used', 175, 'Silver', 2003, to_date(('2023-12-10'),('YYYY-MM-DD')), to_date(('2023-12-10'),('YYYY-MM-DD')));
+insert into ITEM_INFO values ('Bread Maker', 'Fairly Used' 68, 'White', 2005, to_date(('2023-12-10'),('YYYY-MM-DD')), to_date(('2023-12-10'),('YYYY-MM-DD')));
+insert into ITEM_INFO values ('Carpet', 'Used', 140, 'Brown', 1990, to_date(('2023-12-10'),('YYYY-MM-DD')), to_date(('2023-12-10'),('YYYY-MM-DD')));
+insert into ITEM_INFO values ('Levis Jeans', 'Fairly Used', 125, 'Blue', 2000, to_date(('2023-12-10'),('YYYY-MM-DD')), to_date(('2023-12-10'),('YYYY-MM-DD')));
+insert into ITEM_INFO values ('Lamp', 'Used', 136, 'Multicolored', 1989, to_date(('2023-12-10'),('YYYY-MM-DD')), to_date(('2023-12-10'),('YYYY-MM-DD')));
+insert into ITEM_INFO values ('Boombox', 'Fairly used', 202, 'Grey', 2000, to_date(('2023-12-10'),('YYYY-MM-DD')), to_date(('2023-12-10'),('YYYY-MM-DD')));
+insert into ITEM_INFO values ('Side Table', 'Fairly used', 180, 'Brown', 1992, to_date(('2023-12-10'),('YYYY-MM-DD')), to_date(('2023-12-10'),('YYYY-MM-DD')));
+insert into ITEM_INFO values ('Vintage desk', 'Fairly used', 170, 'Brown', 1990, to_date(('2023-12-10'),('YYYY-MM-DD')), to_date(('2023-12-10'),('YYYY-MM-DD')));
