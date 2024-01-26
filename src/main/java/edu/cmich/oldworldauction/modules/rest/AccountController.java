dd144b40bdf941e624599e718dc8ed8717bf9a28
Jones3bc <Jones3bc@cmich.edu>
2024-01-26 11:23:29 -0500

    Improving AccountController by adding endpoints related to Payment Methods.

--- src/main/java/edu/cmich/oldworldauction/modules/rest/AccountController.java
+++ src/main/java/edu/cmich/oldworldauction/modules/rest/AccountController.java
@@ -1,5 +1,7 @@
 package edu.cmich.oldworldauction.modules.rest;
 
+import edu.cmich.oldworldauction.modules.data.AccountDao;
+import edu.cmich.oldworldauction.modules.models.PaymentMethod;
 import edu.cmich.oldworldauction.modules.services.AccountService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,22 +10,56 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
 /**
- * Handles all requests and responses having to do with accounts/account information.
+ * Handles all requests and responses having to do with payment method information.
  */
 @RequestMapping("/")
 @Controller
 public class AccountController {
-    private final AccountService accountService;
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    AccountDao accountDao;
+
+    /**
+     * Creates a new payment method.
+     *
+     * @param paymentMethod The {@link PaymentMethod} to create.
+     */
+    @PostMapping("/create-payment-method")
+    public void createPaymentMethod(@RequestBody PaymentMethod paymentMethod){
+        this.accountDao.insertPaymentMethod(paymentMethod);
+    }
+
+    /**
+     * Retrieves a {@link PaymentMethod} given its ID.
+     *
+     * @param id The ID of the {@link PaymentMethod}
+     * @return The retrieved {@link PaymentMethod}.
+     */
+    @GetMapping("/retrieve-payment-method/{id}")
+    public PaymentMethod retrievePaymentMethodById(@PathVariable int id) {
+        return this.accountDao.retrievePaymentMethod(id);
+    }
+
+    /**
+     * Retrieves all {@link PaymentMethod}s that belong to a specified user.
+     *
+     * @param username The username of the user.
+     * @return A {@link List} of the retrieved {@link PaymentMethod}s.
+     */
+    @GetMapping("/retrieve-payment-methods/{username}")
+    public List<PaymentMethod> retrieveUserPaymentMethods(@PathVariable String username){
+        return this.accountDao.retrieveAllPaymentMethodsForUser(username);
+    }
 
     @GetMapping("/create")
     public String auctionPage(){
         return "Account";
-    }
-
-    @Autowired
-    public AccountController(AccountService accountService) {
-        this.accountService = accountService;
     }
 
     @PostMapping("/create")
