5aad0bff8aaf6acfc2bde42a64eb3bf1251ab3a9
simdi444 <139267957+simdi444@users.noreply.github.com>
2024-02-21 11:50:53 -0500

    Update SeniorDesignTable.sql
    Changes to implement itemID

--- src/main/resources/static/SeniorDesignTable.sql
+++ src/main/resources/static/SeniorDesignTable.sql
@@ -2,35 +2,25 @@ set echo on
 create table USERS(
 	username varchar(30) PRIMARY KEY,
 	password varchar(30) NOT NULL,
-    userID number
+    	userID varchar(40)
 );
 
 commit;
 --desc is used to show the table and all of its contents
 desc USERS;
 
---Commented out section for the account info
---create table ACC_INFO(
---	Fname varchar2(15),
---	Lname varchar2(15),
---	constraint ACC_INFO_fk foreign key (Fname) references USER_INFO(userID)
---);
-
---commit; 
---desc ACC_INFO
-
 create table AUCTION_ITEMS(
 	name varchar(40),
 	description varchar(30),
 	currentBid varchar(30),
-    	itemID number,
+   	 itemID varchar(40),
 	aucStartTime datetime,
 	aucEndTime datetime,
 	sellerUser varchar(30),
 	bidderUser varchar(30),
-    	image blob,
-	FOREIGN KEY(sellerUser) REFERENCES USERS(username),
-	FOREIGN KEY(bidderUser) REFERENCES USERS(username)
+   	 image blob,
+	FOREIGN KEY(sellerUser) REFERENCES USERS(userID),
+	FOREIGN KEY(bidderUser) REFERENCES USERS(userID)
 );
 
 commit;
@@ -38,14 +28,17 @@ desc AUCTION_ITEMS;
 
 --credit 0 or 1 for boolean
 create table PAYMENT_METHODS(
-    id varchar(10),
+    paymentID varchar(40),
     credit integer,
     cardNumber varchar(30),
     expirationMonth number,
     expirationYear number,
     cvv number,
-    userUsername varchar(30),
-    FOREIGN KEY (userUsername) REFERENCES USERS(username)
+    userID varchar(40),
+    FOREIGN KEY (userID) REFERENCES USERS(userID)
 );
+
+commit;
+desc PAYMENT_METHODS;
 
 purge recyclebin;
