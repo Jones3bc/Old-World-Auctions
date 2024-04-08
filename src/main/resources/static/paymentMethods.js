efeacbf9f7f7e83d836612ddc70d8a79cc620140
Jones3bc <Jones3bc@cmich.edu>
2024-04-08 11:56:46 -0400

    Payment method additions and cleaning up extra files. Still a little more to go on payment methods

--- src/main/resources/static/paymentMethods.js
+++ src/main/resources/static/paymentMethods.js
@@ -12,11 +12,6 @@ function checkPassword() {
     fetch("/check-password?password=" + password)
         .then(response => response.json())
         .then(data => {
-        console.log(data.isValid);
-        console.log(data.isValid == "true");
-        console.log(data.isValid == true);
-        console.log(data.isValid === "true");
-        console.log(data.isValid === true);
         if(data.isValid == "true") {
             const paymentMethodDiv = document.getElementById("paymentMethods");
             paymentMethodDiv.innerHTML = "";
@@ -27,28 +22,51 @@ function checkPassword() {
                 fetch("/retrieve-payment-methods?userId=" + dataTwo.userId)
                     .then(response => response.json())
                     .then(dataThree => {
+                    paymentMethodDiv.innerHTML = `
+                        <form style="margin-left: 100px auto;">
+                            <input value="Card Number" style="padding: 15px; outline: none;" disabled/>
+                            <input value="Expiration Month" style="padding: 15px; outline: none;" disabled/>
+                            <input value="Expiration Year" style="padding: 15px; outline: none;" disabled/>
+                            <input value="CVV" style="padding: 15px; outline: none;" disabled/>
+                            <select style="padding: 15px" disabled>
+                                <option>Is this a credit card?</option>
+                            </select>
+                        </form>
+                    `;
                     dataThree.forEach(paymentMethod => {
                         const paymentMethodForm = document.createElement("form");
+                        paymentMethodForm.setAttribute("method", "post");
+                        paymentMethodForm.setAttribute("action", "/update-payment-method");
 
                         const paymentMethodParagraph = document.createElement("p");
 
                         const paymentMethodNumber = document.createElement("input");
                         paymentMethodNumber.setAttribute("type", "text");
+                        paymentMethodNumber.setAttribute("name", "cardNumber")
                         paymentMethodNumber.value = paymentMethod.cardNumber;
+                        paymentMethodNumber.style.padding = "15px";
 
                         const paymentMethodExpMonth = document.createElement("input");
                         paymentMethodExpMonth.setAttribute("type", "number");
+                        paymentMethodExpMonth.setAttribute("name", "expirationMonth");
                         paymentMethodExpMonth.value = paymentMethod.expirationMonth;
+                        paymentMethodExpMonth.style.padding = "15px";
 
                         const paymentMethodExpYear = document.createElement("input");
                         paymentMethodExpYear.setAttribute("type", "number");
+                        paymentMethodExpYear.setAttribute("name", "expirationYear")
                         paymentMethodExpYear.value = paymentMethod.expirationYear;
+                        paymentMethodExpYear.style.padding = "15px"
 
                         const paymentMethodCvv = document.createElement("input");
                         paymentMethodCvv.setAttribute("type", "number");
+                        paymentMethodCvv.setAttribute("name", "cvv")
                         paymentMethodCvv.value = paymentMethod.cvv;
+                        paymentMethodCvv.style.padding = "15px";
 
                         const paymentMethodIsCredit = document.createElement("select");
+                        paymentMethodIsCredit.setAttribute("name", "credit");
+                        paymentMethodIsCredit.style.padding = "15px";
 
                         const paymentMethodYes = document.createElement("option");
                         paymentMethodYes.value = "Yes";
@@ -61,19 +79,23 @@ function checkPassword() {
                         paymentMethodIsCredit.appendChild(paymentMethodYes);
                         paymentMethodIsCredit.appendChild(paymentMethodNo);
 
-                        console.log(paymentMethod.credit);
                         if(paymentMethod.credit == true) {
                             paymentMethodIsCredit.value = "Yes";
                         } else {
                             paymentMethodIsCredit.value = "No";
                         }
 
+                        const paymentMethodButton = document.createElement("button");
+                        paymentMethodButton.innerHTML = "Update";
+                        paymentMethodButton.setAttribute("type", "submit");
+                        paymentMethodButton.className = "submit";
 
                         paymentMethodParagraph.appendChild(paymentMethodNumber);
                         paymentMethodParagraph.appendChild(paymentMethodExpMonth);
                         paymentMethodParagraph.appendChild(paymentMethodExpYear);
                         paymentMethodParagraph.appendChild(paymentMethodCvv);
                         paymentMethodParagraph.appendChild(paymentMethodIsCredit);
+                        paymentMethodParagraph.appendChild(paymentMethodButton);
 
                         paymentMethodForm.appendChild(paymentMethodParagraph);
 
