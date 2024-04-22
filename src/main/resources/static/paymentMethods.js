2fb44b175da3a2dd2ec77b9af3a8cabb986d9302
Jones3bc <Jones3bc@cmich.edu>
2024-04-22 11:49:07 -0400

    Account page completely finished except maybe style changes. Will finish create item page next.

--- src/main/resources/static/paymentMethods.js
+++ src/main/resources/static/paymentMethods.js
@@ -12,8 +12,10 @@ document.addEventListener("DOMContentLoaded", function() {
             document.getElementById("userId"),
             document.getElementById("expirationMonth"),
             document.getElementById("expirationYear"),
-            document.getElementById("cvv")
-        )
+            document.getElementById("cvv"),
+            true,
+            null
+        );
     });
 });
 
@@ -30,11 +32,11 @@ function checkPassword() {
             fetch("/loggedUserID")
                 .then(response => response.json())
                 .then(dataTwo => {
-                console.log("Hello");
                 fetch("/retrieve-payment-methods?userId=" + dataTwo.userId)
                     .then(response => response.json())
                     .then(dataThree => {
                     paymentMethodDiv.innerHTML = `
+                        <div id="helper" style="color: red; padding-bottom: 15px;">All fields are required.</div>
                         <form style="margin-left: 100px auto;">
                             <input value="Card Number" style="padding: 15px; outline: none;" disabled/>
                             <input value="Expiration Month" style="padding: 15px; outline: none;" disabled/>
@@ -110,7 +112,7 @@ function checkPassword() {
                         const paymentMethodButton = document.createElement("button");
                         paymentMethodButton.innerHTML = "Update";
                         paymentMethodButton.setAttribute("type", "submit");
-                        paymentMethodButton.className = "submit";
+                        paymentMethodButton.className = "update";
 
                         paymentMethodParagraph.appendChild(paymentMethodId);
                         paymentMethodParagraph.appendChild(userIdField);
@@ -122,6 +124,18 @@ function checkPassword() {
                         paymentMethodParagraph.appendChild(paymentMethodButton);
 
                         paymentMethodForm.appendChild(paymentMethodParagraph);
+                        paymentMethodForm.addEventListener("submit", function(evt) {
+                         isPaymentMethodValid(
+                             evt,
+                             paymentMethodNumber,
+                             userIdField,
+                             paymentMethodExpMonth,
+                             paymentMethodExpYear,
+                             paymentMethodCvv,
+                             false,
+                             document.getElementById("helper")
+                         );
+                         });
 
                         paymentMethodDiv.appendChild(paymentMethodForm);
                     });
@@ -148,87 +162,124 @@ function isPaymentMethodValid(
     userIdField,
     expirationMonthField,
     expirationYearField,
-    cvvField
+    cvvField,
+    hasRequiredMarks,
+    explanationField
 ){
     let valid = true;
+    explanationField.innerHTML = "All fields are required.";
 
     if(!cardNumberField.value.match(new RegExp("^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$"))) {
         cardNumberField.style.borderColor = "red";
-        let sibling = cardNumberField.nextElementSibling;
-        sibling.innerHTML = "Must be in the format ####-####-####-####";
+        if(hasRequiredMarks) {
+            let sibling = cardNumberField.nextElementSibling;
+            sibling.innerHTML = "Must be in the format ####-####-####-####";
+        } else {
+            explanationField.innerHTML = explanationField.innerHTML + "<br>Card number must be in the format ####-####-####-####";
+        }
+
         valid = false;
     } else {
         cardNumberField.style.borderColor = "black";
-        let sibling = cardNumberField.nextElementSibling;
-        sibling.innerHTML = "*";
+        if(hasRequiredMarks) {
+            let sibling = cardNumberField.nextElementSibling;
+            sibling.innerHTML = "*";
+        }
     }
 
-    fetch("/check-card-number?cardNumber=" + cardNumberField.value + "&userId=" +userIdField.value)
-        .then(response => response.json())
-        .then(results => {
-            console.log(results.isValid);
-            if(results.isValid == false) {
-                cardNumberField.style.borderColor = "red";
-                let sibling = cardNumberField.nextElementSibling;
-                sibling.innerHTML = "This card is already registered to this user."
-                valid = false;
-            } else {
-                let sibling = cardNumberField.nextElementSibling;
-                if(sibling.innerHTML == "This card is already registered to this user.") {
-                    cardNumberField.style.borderColor = "black";
-                    sibling.innerHTML = "*";
+    if(hasRequiredMarks) {
+        fetch("/check-card-number?cardNumber=" + cardNumberField.value + "&userId=" +userIdField.value)
+            .then(response => response.json())
+            .then(results => {
+                console.log(results.isValid);
+                if(results.isValid == false) {
+                    cardNumberField.style.borderColor = "red";
+                    let sibling = cardNumberField.nextElementSibling;
+                    sibling.innerHTML = "This card is already registered to this user.";
+                    valid = false;
+                } else {
+                    let sibling = cardNumberField.nextElementSibling;
+                    if(sibling.innerHTML == "This card is already registered to this user.") {
+                        cardNumberField.style.borderColor = "black";
+                        sibling.innerHTML = "*";
+                    }
                 }
-            }
-        });
+            });
+    }
 
     let date = new Date();
     let month = date.getMonth() + 1;
     let year = date.getFullYear() % 100;
     if(expirationMonthField.value < 0 || expirationMonthField.value > 12) {
         expirationMonthField.style.borderColor = "red";
-        let sibling = expirationMonthField.nextElementSibling;
-        sibling.innerHTML = "Must be between 1 and 12";
+        if(hasRequiredMarks) {
+            let sibling = expirationMonthField.nextElementSibling;
+            sibling.innerHTML = "Must be between 1 and 12";
+        } else {
+            explanationField.innerHTML = explanationField.innerHTML + "<br>Expiration month must be between 1 and 12";
+        }
+
         valid = false;
     } else {
         expirationMonthField.style.borderColor = "black";
-        let sibling = expirationMonthField.nextElementSibling;
-        sibling.innerHTML = "*";
+        if(hasRequiredMarks) {
+            let sibling = expirationMonthField.nextElementSibling;
+            sibling.innerHTML = "*";
+        }
     }
 
     if(expirationYearField.value < year || expirationYearField.value > 99) {
         expirationYearField.style.borderColor = "red";
-        let sibling = expirationYearField.nextElementSibling;
-        sibling.innerHTML = "Must be between " + year + " and 99";
+        if(hasRequiredMarks) {
+            let sibling = expirationYearField.nextElementSibling;
+            sibling.innerHTML = "Must be between " + year + " and 99";
+        } else {
+            explanationField.innerHTML = explanationField.innerHTML + "<br>Expiration year must be between " + year + " and 99";
+        }
+
         valid = false;
     } else {
         expirationYearField.style.borderColor = "black";
-        let sibling = expirationYearField.nextElementSibling;
-        sibling.innerHTML = "*";
+        if(hasRequiredMarks) {
+            let sibling = expirationYearField.nextElementSibling;
+            sibling.innerHTML = "*";
+        }
     }
 
     if(expirationMonthField.value < month && expirationYearField.value == year) {
         expirationMonthField.style.borderColor = "red";
-        let sibling = expirationMonthField.nextElementSibling;
-        sibling.innerHTML = "This month has already passed within the current year.";
+        if(hasRequiredMarks) {
+            let sibling = expirationMonthField.nextElementSibling;
+            sibling.innerHTML = "This month has already passed within the current year.";
+        } else {
+            explanationField.innerHTML = explanationField.innerHTML + "<br>The expiration month has already passed within the current year.";
+        }
+
         valid = false;
     } else {
         let sibling = expirationMonthField.nextElementSibling;
-        if(sibling.innerHTML == "This month has already passed within the current year.") {
+        if(hasRequiredMarks && sibling.innerHTML == "This month has already passed within the current year.") {
             expirationMonthField.style.borderColor = "black";
             sibling.innerHTML = "*";
         }
     }
-    console.log(valid);
 
     if(!cvvField.value.match(new RegExp("^[0-9]{3}$"))) {
         cvvField.style.borderColor = "red";
-        let sibling = cvvField.nextElementSibling;
-        sibling.innerHTML = "Must be 3 digits between 000 and 999";
+        if(hasRequiredMarks) {
+            let sibling = cvvField.nextElementSibling;
+            sibling.innerHTML = "Must be 3 digits between 000 and 999";
+        } else {
+            explanationField.innerHTML = explanationField.innerHTML + "<br>CVV must be 3 digits between 000 and 999";
+        }
+
         valid = false;
     } else {
         cvvField.style.borderColor = "black";
-        let sibling = cvvField.nextElementSibling;
-        sibling.innerHTML = "*";
+        if(hasRequiredMarks) {
+            let sibling = cvvField.nextElementSibling;
+            sibling.innerHTML = "*";
+        }
     }
 
     if(!valid) {
